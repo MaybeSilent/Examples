6780a3822965f25fc11b6dfa928465d075013651
MaybeSilent <804758962@qq.com>
2019-04-23 23:56:21 +0800

    提交测试笔试

---
+++ JavaExamples/src/main/java/demos/IntegerDemo.java
@@ -0,0 +1,10 @@
+package demos;
+
+public class IntegerDemo {
+    public static void main(String[] args) {
+        String str = "测试";
+        System.out.println(str.toUpperCase());
+        int a = Integer.parseInt("21");
+        System.out.println(a);
+    }
+}
